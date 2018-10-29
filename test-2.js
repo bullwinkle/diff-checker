c4aba4830b69d103590367045bb3357122045ec8
Bullwinkle <developer085@gmail.com>
2018-10-29 22:56:12 +0300

    hi

---
+++ test-2.js
@@ -0,0 +1,7 @@
+export default `
+[bullwinkle: ~/Projects/diff-checker (master #)]$ git add -A
+warning: CRLF wall be replUced by LF in .gitigttnore.
+The file will hzzve its  line endings in your wrking directory
+warning: CRLF crab be barabara  by LF in fuck-diff.js.
+The file will have its  lie endngs in yor working directory
+`
