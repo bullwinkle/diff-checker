f114d3ff0849ae79184d4d23ffa216fe7c05e9e0
Bullwinkle <developer085@gmail.com>
2018-10-30 11:43:56 +0300

    simple ready

--- test-2.js
+++ test-2.js
@@ -1,7 +1,8 @@
 export default `
-[bullwinkle: ~/Projects/diff-checker (master #)]$ git add -A
-warning: CRLF wall be replUced by LF in .gitigttnore.
-The file will hzzve its  line endings in your wrking directory
-warning: CRLF crab be barabara  by LF in fuck-diff.js.
-The file will have its  lie endngs in yor working directory
+Hang the boy, can't I never learn anything? Ain't he played me tricks enough like that for me to be looking out 
+for him by this time? But old fools is the biggest fools there is. Can't learn an old dog new tricks, as the 
+saying is. But my goodness, he never plays them alike, two days, and how is a body to know what's coming? 
+He 'pears to know just how long he can torment me before I get my dander up, and he knows if he can make out to 
+put me off for a minute or make me laugh, it's all down again and I can't hit him a lick. I ain't doing my duty by 
+that boy, and that's the Lord's truth, goodness knows. Spare the rod and spile the child, as the Good Book says. 
 `
